5931bf39e31b89fc4177a26877c043432cab3c91
Varsha C L <clvarsha1998@gmail.com>
2020-02-19 13:50:36 +0530

    fix:css for song card

--- src/Components/SongCard/index.jsx
+++ src/Components/SongCard/index.jsx
@@ -1,20 +1,31 @@
 
 import React from 'react';
 import propTypes from 'prop-types';
+import { IoMdHeart } from 'react-icons/io';
 import * as styles from './index.module.css';
 
 const SongCard = ({ albumArtUrl, artists, albumName }) => (
   <div className={styles.songCardContainer}>
+
     <div>
       <img src={albumArtUrl} alt={albumName} className={styles.albumPicture} />
     </div>
-    <div className={styles.albumName}>
-      {albumName}
-    </div>
-    <div className={styles.artists}>
-      {artists.toString()}
+    <div className={styles.albumDetails}>
+      <div className={styles.albumNameArtists}>
+        <div className={styles.albumName}>
+          {albumName}
+        </div>
+        <div className={styles.artists}>
+          {artists.toString()}
+        </div>
+      </div>
+      <div className={styles.love}>
+        <IoMdHeart />
+      </div>
     </div>
   </div>
+
+
 );
 
 export default SongCard;
